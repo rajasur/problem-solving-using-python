5041b78712fe0cca99ee76fb70047cfbb3f8d6f1
rajasur <colab.raja@gmail.com>
2024-11-24 13:32:36 +0530

    left rotation by one place program

---
+++ 5.left-rotate-array-oneplace.py
@@ -0,0 +1,16 @@
+from typing import List 
+
+def leftRotateByOnePlace(arr: List['int'], n: int) -> List['int']:
+    temp = arr[0] 
+    for i in range(1, n):
+        arr[i-1] = arr[i] 
+    arr[n-1] = temp 
+    return arr 
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5,6,7]
+    print(f"Before Left Rotation {arr}")
+    n = len(arr)
+    res_left_rotation = leftRotateByOnePlace(arr, n)
+    print(f"After Left Rotation by One Place {res_left_rotation}")
+     
