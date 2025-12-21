5b836d6a0c5ef0614ff76cbf65f0641f2aaff7b0
I571352 <raja.sur@sap.com>
2025-12-21 22:01:47 +0530

    Add implementation of Valid Palindrome problem with examples and logic

---
+++ NeetCode150/2_Two Pointers/1_Valid Palindrome/program.py
@@ -0,0 +1,63 @@
+#TODO - 125. Valid Palindrome
+"""
+Easy
+Topics
+premium lock icon
+Companies
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+
+from typing import List
+ 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left , right = 0, len(s) - 1 
+        while left < right:
+            while left < right and not self.isAlphaNumeric(s[left]):
+                left += 1 
+            while left < right and not self.isAlphaNumeric(s[right]):
+                right -= 1 
+            if s[left].lower() != s[right].lower():
+                return False 
+            left += 1 
+            right -= 1 
+        return True
+        
+    def isAlphaNumeric(self, c: str) -> bool:
+        return (
+                ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9')
+        )
+
+if __name__ == "__main__":
+    s = "A man, a plan, a canal: Panama"
+    sol = Solution()
+    print(sol.isPalindrome(s))
